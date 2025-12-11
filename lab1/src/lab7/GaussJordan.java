d666e522a9b39a84638e83e570b6e7e3830f4345
lythx <lythxxx@gmail.com>
2025-12-11 11:40:51 +0100

    lab7 gauss jordan solver

--- lab1/src/lab7/GaussJordan.java
+++ lab1/src/lab7/GaussJordan.java
@@ -1,30 +1,66 @@
 package lab7;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class GaussJordan {
 
-    private final int n;
+    private final int size;
     private final double[][] matrix;
-    private final double[][] multipliers;
-    private final double[][] subtractors;
+    private final double[] multipliers;
+    private final double[][] valuesToSubtract;
     private final ExecutorService executorService;
 
     public GaussJordan(double[][] matrix) {
-        n = matrix.length - 1;
+        size = matrix.length;
         this.matrix = matrix;
-        this.multipliers = new double[n + 1][n];
-        this.subtractors = new double[n + 1][n];
+        this.multipliers = new double[size];
+        this.valuesToSubtract = new double[size][size + 1];
 
         var threadCount = Runtime.getRuntime().availableProcessors();
         executorService = Executors.newFixedThreadPool(threadCount);
     }
 
     public void run() {
+        try {
+            List<Future<?>> results = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                for (int k = i + 1; k < size; k++) {
+                    results.add(executorService.submit(new TaskA(i, k, matrix, multipliers)));
+                }
+                for (var result : results) {
+                    result.get();
+                }
+                results.clear();
 
+
+                for (int k = i + 1; k < size; k++) {
+                    for (int j = i; j < size + 1; j++) {
+                        results.add(executorService.submit(new TaskB(i, j, k, matrix, multipliers, valuesToSubtract)));
+                    }
+                }
+                for (var result : results) {
+                    result.get();
+                }
+                results.clear();
+
+                for (int k = i + 1; k < size; k++) {
+                    for (int j = i; j < size + 1; j++) {
+                        results.add(executorService.submit(new TaskC(j, k, matrix, valuesToSubtract)));
+                    }
+                }
+                for (var result : results) {
+                    result.get();
+                }
+                results.clear();
+            }
+        } catch (ExecutionException | InterruptedException ex) {
+            System.out.println("GaussJordan error: " + ex.getMessage());
+        }
+        finally {
+            executorService.shutdown();
+        }
     }
-
-
 
 }
--- lab1/src/lab7/Main.java
+++ lab1/src/lab7/Main.java
@@ -1,9 +1,23 @@
 package lab7;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        double[][] matrix = {
+            {2, 1, 3, 6, 10, 100},
+            {4, 3, 8, 15, 15, 102},
+            {6, 5, 16, 27, 17, 405},
+            {5, 7, 12, 13, 56, 12},
+            {1, 2, 4, 6, 12, 44}
+        };
 
+        var gj = new GaussJordan(matrix);
+        gj.run();
+        for (var row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 
 }
